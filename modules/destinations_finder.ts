171ee03024d99d6a593ede9a2594097c834e7ab0
Gilad Pomer <giladpomer@gmail.com>
2022-12-11 12:31:13 +0100

    destinations finder logic

--- modules/destinations_finder.ts
+++ modules/destinations_finder.ts
@@ -1,6 +1,22 @@
 import { Destination } from "../types/destination";
 import { HotelInfo } from "../types/hotel-info";
 
+import deepEqual = require('deep-equal');
+
 export function find(hotelsInfo: HotelInfo[]): Destination[] {
-    return [];
+    const destinations: Destination[] = [];
+
+    hotelsInfo.forEach(hotelInfo => {
+        const destination: Destination = {
+            city: hotelInfo.city,
+            country: hotelInfo.country
+        };
+
+        //add if does not already exist
+        if (!destinations.some(item => deepEqual(item, destination))) {
+            destinations.push(destination);
+        }
+    });
+
+    return destinations;
 }
