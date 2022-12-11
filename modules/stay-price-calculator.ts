29b5e13e504f312d9d52d114241721f467fd0261
Gilad Pomer <giladpomer@gmail.com>
2022-12-11 15:52:18 +0100

    stay price calculator logic

--- modules/stay-price-calculator.ts
+++ modules/stay-price-calculator.ts
@@ -1,7 +1,17 @@
+const DAY_IN_MILLISECONDS: number = 1000 * 60 * 60 * 24;
+
 export function calculateStayPrice(
     pricePerNight: number,
     checkInDate: Date,
     checkOutDate: Date): number {
+    const nights: number = calculateNightsAmount(checkInDate, checkOutDate);
 
-    return 0;
+    return nights * pricePerNight;
+}
+
+function calculateNightsAmount(
+    checkInDate: Date,
+    checkOutDate: Date): number {
+    const timeSpanInMilliseconds: number = checkOutDate.getTime() - checkInDate.getTime();
+    return Math.round(timeSpanInMilliseconds / DAY_IN_MILLISECONDS);
 }
