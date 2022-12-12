0d28ee61772b84713d2e5fa0efc435a30ae10c05
Gilad Pomer <giladpomer@gmail.com>
2022-12-12 12:02:59 +0100

    cors origin depending on env

--- server.ts
+++ server.ts
@@ -1,11 +1,18 @@
 import express = require('express');
+import cors = require('cors');
+
 import destinationsRouter = require('./endpoints/destinations');
 import searchRouter = require('./endpoints/search');
 
 const app = express();
 const port = process.env.PORT || 1337;
+const clientOrigin = process.env.CLIENT_ORIGIN || '*';
 
 app.use(express.json());
+app.use(cors({
+    origin: clientOrigin
+}));
+
 app.use('/destinations', destinationsRouter);
 app.use('/search', searchRouter);
 
