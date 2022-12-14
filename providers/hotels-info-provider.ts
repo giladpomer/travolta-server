c9efe3af388aca6fffd5b4dbd81526501326f6d2
Gilad Pomer <giladpomer@gmail.com>
2022-12-14 18:37:22 +0100

    supporting availability dates from json in hotels info provider

--- providers/hotels-info-provider.ts
+++ providers/hotels-info-provider.ts
@@ -3,5 +3,15 @@ import { HotelInfo } from '../types/hotel-info';
 import fs = require('fs');
 
 export function getHotelsInfo(): HotelInfo[] {
-    return JSON.parse(fs.readFileSync('data/hotels.json', 'utf8'));
+    return JSON.parse(
+        fs.readFileSync('data/hotels.json', 'utf8'),
+        availabilityDatesConverter);
+}
+
+function availabilityDatesConverter(key, value) {
+    if (typeof value === 'string' && (key === 'from' || key === 'until')) {
+        return new Date(value);
+    }
+
+    return value;
 }
